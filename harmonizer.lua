1d96544b6cce6f2b2b05d75d4c49e49cac3efc72
Joseph Constantakis <jcon5294@gmail.com>
2015-04-01 20:30:48 -0400

    update interface to allow selection of harmonizer range

--- harmonizer.lua
+++ harmonizer.lua
@@ -2,10 +2,17 @@ FreeAllRegions()
 FreeAllFlowboxes()
 DPrint("")
 
+SOPRANO = 0
+BASS = 1
+OCTAVES = {}
+OCTAVES[SOPRANO] = 1
+OCTAVES[BASS] = -1
+
 function main()
 	createPage1()
 	createPage2()
-	createButtons()
+	createHarmonizerPage(0)
+	createHarmonizerPage(1)
 	--createFlowboxes()
 	SetPage(1)
 end
@@ -30,16 +37,8 @@ end
 
 -- Navigate from page one function
 function switchPageOnPress(region)
-	if(region.id + 1 < 4) then
-		SetPage(region.id + 1)
-		createFlowboxes()
-		
-		--[[ In the future we will populate these pages with pages that look like page 3 for t
-		he different singers but the buttons on these pages will be connected to different
-		pitch shifting function --]]
-	else
-		DPrint("These pages haven't been created yet")
-	end
+	SetPage(region.id + 1)
+	createFlowboxes()
 end
 
 -- Navigate back to page one function
@@ -72,28 +71,31 @@ end
 -- Harmony Page
 -- Buttons --
 btns = {}
-selectedButton = 0
+btns[SOPRANO] = {}
+btns[BASS] = {}
+selectedButton = nil
 currentfrequency = 1
 
 -- Select harmony function
 function selectButton(btn)
-	if btn.id == selectedButton then
-		deselectButton(btns[selectedButton])
+	if selectedButton ~= nil and
+	   btn.id == selectedButton.id and 
+	   btn.singerType == selectedButton.singerType then
+		deselectButton(selectedButton)
 		--push2:Push(0)
-		selectedButton = 0
+		selectedButton = nil
 		FreeAllFlowboxes()
 		return
 	end
 	btn.t:SetSolidColor(50,200,50,255)
-	if selectedButton ~= 0 then
-		deselectButton(btns[selectedButton])
+	if selectedButton ~= nil then
+		deselectButton(selectedButton)
 	else 
 		createFlowboxes()
 	end
 	
-	selectedButton = btn.id
-	semitones = btn.id - 1
-	shift = shiftBySemitones(-semitones)
+	selectedButton = btn
+	shift = shiftBySemitones(btn.shift)
 	
 	push2:Push(shift)
 	-- In the future this will connect to a pitch changing function
@@ -105,8 +107,8 @@ function deselectButton(btn)
 	-- In the future this will detach the current flowbox
 end
 
-function createButtons()
-	SetPage(3)
+function createHarmonizerPage(singerType)
+	SetPage(3 + singerType)
 	labels = {"Unison","m2","2","m3","3","4","d5","5","m6","6","m7","7","Octave"}
 	coords = {{0,0},{0,1},{1,1},{0,2},{1,2},{1,3},{0,4},{1,4},{0,5},{1,5},{0,6},{1,6},{0,7}}
 	widths = {1,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1}
@@ -126,7 +128,9 @@ function createButtons()
 	
 	for i=1,13 do
 		local btn = Region()
+		btn.singerType = singerType
 		btn.id = i
+		btn.shift = (btn.id-1)*OCTAVES[singerType]
 		btn.t = btn:Texture(240, 240, 240, 255)
 		btn.t:SetBlendMode("BLEND")
 		width = widths[i]
@@ -152,8 +156,8 @@ page1btns = {}
 function createPage1()
 	SetPage(1)
 	FreeAllRegions()
-	labels = {"Singer", "Soprano", "Alto", "Tenor", "Baritone", "Bass"}
-	coords = {.7, .6, .5, .4, .3, .2}
+	labels = {"Singer", "Soprano", "Bass"}
+	coords = {.7, .6, .5}
 	
 	WriteURLData("http://www.natomasarts.com/wp/wp-content/uploads/2011/07/microphone_black_background2.jpg", "mic.jpg")
 	background = Region()
@@ -163,7 +167,7 @@ function createPage1()
 	background.t:SetTexCoord(0, npow2ratio(background.t:Width()), npow2ratio(background.t:Height()), 0)
 	background:Show()
 	
-	for i=1,6 do
+	for i=1,#labels do
 		local btn = Region()
 		btn:SetHeight(40)
 		btn.t1 = btn:TextLabel()
